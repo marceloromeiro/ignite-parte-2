f574563e743ebce25cf99bdebe256587ab994b22
Marcelo de Souza Romeiro <marcelo.romeiro@zema.com>
2021-10-25 18:51:16 -0300

    25/10

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,12 @@
+import { Summary } from "../Summary";
+import { Container } from "./styles";
+import {TransactionsTable} from '../TransactionTable'
+
 export function Dashboard(){
     return (
-
+        <Container>
+            <Summary/>
+            <TransactionsTable />
+        </Container>
     );
 }
