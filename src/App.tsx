17eda0ad6f5ba54894ac32e1a07b017362126879
Marcelo de Souza Romeiro <marcelo.romeiro@zema.com>
2021-10-27 18:53:12 -0300

    27/10

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import {GlobalStyle} from './styles/global'
 import {Header} from './components/Header'
 import {Dashboard} from './components/Dashboard'
+import {NewTransactionModal} from './components/NewTransactionModal'
+import Modal from 'react-modal';
+import { useState } from 'react';
+
+
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+}
+
+function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+}
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard/>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
      <GlobalStyle />
     </>
   );
